8ca4b1eb947e7ff8e0c5b18318a4c61917f6530c
Leohermano <164542030+Leohermano@users.noreply.github.com>
2026-08-27 15:47:18 -0300

    Estudos 2 em lógica

---
+++ 09operadores_logicos/Program.cs
@@ -0,0 +1,12 @@
+﻿// Váriaveis 
+double projeto1 = 7;
+double projeto2 = 5;
+
+// operador E
+Console.WriteLine(projeto1>= 7 && projeto2 >= 7);
+
+// Operador ou
+Console.WriteLine(projeto1>= 7 ||projeto2 >= 7);
+
+// Operador Negação
+Console.WriteLine(!true);
