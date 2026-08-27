a16ea3393955b387b91299117a2930f41996c3e2
Leohermano <164542030+Leohermano@users.noreply.github.com>
2026-08-27 16:30:30 -0300

    atualização

---
+++ 06conversao/Program.cs
@@ -0,0 +1,11 @@
+﻿// Obter o primeiro número
+Console.Write("Digite o primeiro número: ");
+int numero1 = Convert.ToInt16(Console.ReadLine());
+
+// Obter o segundo número
+Console.Write("Digite o segundo número: ");
+int numero2 = Convert.ToInt16(Console.ReadLine());
+
+//Realizar soma
+Console.WriteLine($" A soma é {numero1 + numero2}");
+
