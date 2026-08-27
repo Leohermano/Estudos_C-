203857c21040ae5496e55492ca502cb7c9a79682
Leohermano <164542030+Leohermano@users.noreply.github.com>
2026-08-27 18:16:43 -0300

    tinha faltado

---
+++ 07operadores_aritimeticos/Program.cs
@@ -0,0 +1,26 @@
+﻿//VAriaveis
+int numero1 = 30;
+int numero2 = 3;
+
+//divisão 
+Console.WriteLine("Divisão:" + (numero1/numero2));
+
+//multiplicação
+Console.WriteLine("Multiplicação:" + (numero1*numero2));
+
+//subtração
+Console.WriteLine("Subtração:" + (numero1-numero2));
+
+//adição
+Console.WriteLine("Adição:" + (numero1+numero2));
+
+//Módulo
+Console.WriteLine("Módulo:" + (numero1%numero2));
+
+//Incremento
+numero1++;
+Console.WriteLine("Incremento:" + numero1);
+
+//Decremento
+numero2--;
+Console.WriteLine("Decremento:" + numero2);
